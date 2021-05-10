f38e5f9347a6e7cdc45a2f5ab82968d28ed5d285
Mathilde Aguiar <mathilde.aguiar@etu.univ-grenoble-alpes.fr>
2021-05-10 19:08:07 +0200

    up to the db creation

---
+++ fbapp/config.py
@@ -0,0 +1,11 @@
+# To generate a new secret key:
+#import random, string
+#print("".join([random.choice(string.printable) for _ in range(24)]))
+
+import os
+
+SECRET_KEY = ";fpe6j+anFcilxi}|-i&ioBU"
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+#FB_APP_ID = 1200420960103822
